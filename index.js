5d53f9d419cee63b4b2bb0dc05c1035c09db1e44
shannone <shannone426@gmail.com>
2022-08-05 18:18:58 -0400

    Phase-1-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution in this file!
+
+
+
+
+
+
+
+
+
+//let hQ = 42;
+//let feet = 264;
+const distanceFromHqInBlocks = (blocks) => {
+    if (blocks >= 42) {
+        return blocks - 42;
+    } else if (blocks < 42) {
+        return 42 - blocks;
+    };
+};
+
+//call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+//passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+//the return value of distanceFromHqInBlocks can then be used to calculate feet
+const distanceFromHqInFeet = (feet) => {
+    let distance = distanceFromHqInBlocks(feet);
+    return (distance * 264);
+};
+
+// Calculates the number of feet a passenger travels given a starting block and an ending block 
+// it only calculates distance North and South (uptown/downtown). 
+// It uses the knowledge that a block is 264 feet long.
+const distanceTravelledInFeet = (start, end) => {
+   //const blocks = Math.abs(start, end);
+   //return (blocks * 264);
+   const blocks = Math.abs(start - end);
+   return (blocks * 264);
+ } ;
+
+// Given the same starting and ending block as the previous test (hint hint), 
+// first 400 feet are free
+// distances 400 to 2000 feet are 2 cents per foot not including 400
+// flat fare for over 2000 and under 2500
+// if ride is over 2500 returns "cannot travel that far"
+ const calculatesFarePrice = (start, destination) => {
+    let ride = distanceTravelledInFeet(start, destination);
+    if (ride <= 400) {
+        return 0;
+    } else if (ride <= 2000) {
+        return (ride - 400) * 0.02;
+    } else if (ride >= 2000 && ride <= 2500) {
+        //console.log("third if");
+        return 25;
+    } else if (ride > 2500) {
+        return "cannot travel that far";
+    }
+ };
+//console.log(calculatesFarePrice(34,32));
